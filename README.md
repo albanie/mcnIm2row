19f472a45d11592900a7ca7ab0f2b3981bc6dd06
albanie <albanie@robots.ox.ac.uk>
2018-02-27 17:25:09 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+### mcnIm2row
+
+Expose Matconvnet `im2row` function to MATLAB.  Interface code written
+by James Thewlis.
---
+++ setup_mcnIm2row.m
@@ -0,0 +1,10 @@
+function setup_mcnIm2row()
+%SETUP_MCNIM2ROW Sets up mcnIm2row, by adding its folders
+% to the Matlab path
+%
+% Copyright (C) 2018 James Thewlis and Samuel Albanie
+% Licensed under The MIT License [see LICENSE.md for details]
+
+  root = fileparts(mfilename('fullpath')) ;
+  addpath(root, [root '/matlab'], [root '/misc']) ;
+  addpath( [root '/matlab/mex'], [root '/benchmarks']) ;
